362b57b8ee0b9d8f054c3091ae59f611f21fe7b4
Ajay Patidar <patidarajay769@gmail.com>
2024-07-03 22:35:27 +0530

    Implemented fatory design pattern

---
+++ Factory Design Pattern/src/FactoryPattern/Main.java
@@ -0,0 +1,12 @@
+package FactoryPattern;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape shape = shapeFactory.getShape("SQUARE");
+        shape.draw();
+    }
+}
